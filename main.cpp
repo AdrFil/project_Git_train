a32a997dfcc3d0fd6f2a7e166babaabc27be756d
Adrian Filipiński <me@example.com>
2025-10-07 18:35:00 +0200

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-    int Policz(int a, int b){
-        return a+b;
-    }
     int main() {
         std::cout << "Hello, world!" << std::endl;
-        std::cout << "W pierwszym branchu; 2+3=" << Policz(2,3) << std::endl;
-        return 0;
     }
